c3c6da3171fc244fe0ee81c062410d1fd0a3afc9
Fredrik Vraalsen <fredrik@vraalsen.no>
2011-12-20 10:04:58 +0100

    Lagt til hallo-form

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import play.*;
+import play.data.validation.*;
 import play.mvc.*;
 
 import java.util.*;
@@ -11,6 +12,14 @@ public class Application extends Controller {
 
     public static void index() {
         render();
+    }
+
+    public static void sayHello(@Required String myName) {
+	if (validation.hasErrors()) {
+	    flash.error("Oops, please enter your name!");
+	    index();
+	}
+	render(myName);
     }
 
 }
